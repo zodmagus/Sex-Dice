cfb30f98f9d7ca3ed6327509a85c2b43da77b5e1
zodmagus <39577810+zodmagus@users.noreply.github.com>
2019-11-09 12:04:49 -0600

    Create sexdice.py

---
+++ sexdice.py
@@ -0,0 +1,11 @@
+import random
+import time
+
+responses1 = ['lick','suck','kiss','rub','fuck','cum on','ride','finger','nibble','tease','bite','smack']
+responses2 = ['face','ass','mouth','pussy','dick','toes','thigh']
+
+time.sleep(2)
+
+print(random.choice(responses1))
+
+print(random.choice(responses2))
